d0405d7fbd40cdba5725f02f7874b02f4b5a2836
Sean Sube <seansube@gmail.com>
2018-09-01 17:13:31 -0500

    use env for config
    configure server host address/range, key path, and vpn port from container env

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -8,13 +8,13 @@ echo "Enabling IPv4 Forwarding"
 sysctl -w net.ipv4.conf.all.forwarding=1 || echo "Failed to enable IPv4 Forwarding"
 
 echo "Generating server keys"
-wg genkey | tee /tmp/wg_private_key | wg pubkey
+wg genkey | tee ${KUBEWIRE_KEY_PATH} | wg pubkey
 
 echo "Configuring wireguard"
 ip link add dev wg0 type wireguard
 wg set wg0 private-key /tmp/wg_private_key
-wg set wg0 listen-port 5182 # FIXME move configuration to ConfigMap
-ip address add 10.2.0.0/16 dev wg0 # FIXME move routes to ConfigMap
+wg set wg0 listen-port ${KUBEWIRE_VPN_SERVICE_PORT}
+ip address add ${KUBEWIRE_VPN_HOST_RANGE} dev wg0
 
 echo "Activating wireguard network interface"
 ip link set up dev wg0
@@ -29,4 +29,4 @@ iptables -t nat -A POSTROUTING -o eth0 -j MASQUERADE
 
 # keep running
 echo "Running"
-while true; do wg show wg0; sleep 60; done # FIXME maybe replace with supervisord?
+while true; do wg show; sleep 60; done # FIXME maybe replace with supervisord?
